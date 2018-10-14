98c331c17865aa829b992f8bcb379bdbbdd7d461
Kenneth Young Castro <kenneth.young90@gmail.com>
2018-10-14 22:24:11 +0000

    automatically backed up by learn

--- hashketball.rb
+++ hashketball.rb
@@ -165,7 +165,7 @@ end
 def player_stats(player_name)
   game_hash.each do |location, team_data|
     if team_data[:players].has_key?(player_name)
-      return team_data[:players][player_name][]
+      return team_data[:players][player_name]
     end 
   end 
 end 
