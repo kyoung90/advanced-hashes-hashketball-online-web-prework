c9212c28e76126c987ebbd07746e9556814e3eab
Kenneth Young Castro <kenneth.young90@gmail.com>
2018-10-14 21:17:15 +0000

    automatically backed up by learn

--- hashketball.rb
+++ hashketball.rb
@@ -5,15 +5,15 @@ def game_hash
       team_name: "Brooklyn Nets",
       colors: ["Black, White"],
       players: {
-        "Jeff Adrien": {
-          number: 4,
-          shoe: 18,
-          points: 10,
-          rebounds: 1,
-          assists: 1,
-          steals: 2,
-          blocks: 7,
-          slam_dunks: 2
+        "Alan Anderson": {
+          number: 0,
+          shoe: 16,
+          points: 22,
+          rebounds: 12,
+          assists: 12,
+          steals: 3,
+          blocks: 1,
+          slam_dunks: 1
         }
       }
     }
