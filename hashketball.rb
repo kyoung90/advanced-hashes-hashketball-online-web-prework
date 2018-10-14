3c714a3bc1d6e75b0acf8d676e475b3abde2ce08
Kenneth Young Castro <kenneth.young90@gmail.com>
2018-10-14 21:24:01 +0000

    automatically backed up by learn

--- hashketball.rb
+++ hashketball.rb
@@ -14,6 +14,16 @@ def game_hash
           steals: 3,
           blocks: 1,
           slam_dunks: 1
+        },
+        "Reggie Evans": {
+          number: 0,
+          shoe: 16,
+          points: 22,
+          rebounds: 12,
+          assists: 12,
+          steals: 3,
+          blocks: 1,
+          slam_dunks: 1
         }
       }
     },
