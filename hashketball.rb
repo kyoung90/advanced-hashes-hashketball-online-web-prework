a0528501e2ecb1c72b8c589319179bfd87a0d9a7
Kenneth Young Castro <kenneth.young90@gmail.com>
2018-10-14 22:12:35 +0000

    automatically backed up by learn

--- hashketball.rb
+++ hashketball.rb
@@ -3,7 +3,7 @@ def game_hash
   game = {
     home: {
       team_name: "Brooklyn Nets",
-      colors: ["Black, White"],
+      colors: ["Black", "White"],
       players: {
         "Alan Anderson" => {
           number: 0,
